aa14ae1acb6daa0dd43a82024f74406cd35955b7
Thant Ko Zaw <thantmkou10@gmail.com>
2021-11-03 21:09:55 +0630

    Module can now display graphical interface of chess board

--- lib/modules/displayable.rb
+++ lib/modules/displayable.rb
@@ -1,3 +1,5 @@
+require "pry-byebug"
+
 module Displayable
   class DisplayText
     def instructions
@@ -6,8 +8,18 @@ module Displayable
   end
 
   class DisplayBoard
-    def self.display_board_interface
+    def self.display_board_interface(grid)
       # Implementation Here
+      8.times do |y|
+        8.times do |x|
+          (y + x) % 2 == 0 ? print("\e[41m#{single_square_blueprint(grid[y][x])}\e[0m") : print("\e[47m#{single_square_blueprint(grid[y][x])}\e[0m")
+        end
+        puts
+      end
+    end
+
+    def self.single_square_blueprint(square)
+      square == nil ? '  ' : "#{square.symbol} "
     end
   end
 end
