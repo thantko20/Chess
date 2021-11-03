fc4f170e7243423c0c8dad194b937d5707476836
Thant Ko Zaw <thantmkou10@gmail.com>
2021-11-03 21:32:44 +0630

    Add symbols of different pieces

---
+++ lib/modules/pieces_symbols.rb
@@ -0,0 +1,49 @@
+module PiecesSymbols
+  def black_pawn
+    "\u265f"
+  end
+
+  def white_pawn
+    "\u2659"
+  end
+
+  def black_knight
+    "\u265e"
+  end
+
+  def white_knight
+    "\u2658"
+  end
+
+  def black_bishop
+    "\u265d"
+  end
+
+  def white_bishop
+    "\u2657"
+  end
+
+  def black_rook
+    "\u265c"
+  end
+
+  def white_rook
+    "\u2656"
+  end
+
+  def black_queen
+    "\u265b"
+  end
+
+  def white_queen
+    "\u2655"
+  end
+
+  def black_king
+    "\u2654"
+  end
+
+  def white_king
+    "\u265a"
+  end
+end
