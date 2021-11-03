e3a4f26b5db5f1eb6e322fc07b61196e0bbbe9e1
Thant Ko Zaw <thantmkou10@gmail.com>
2021-11-03 20:26:50 +0630

    Add classes

---
+++ lib/modules/displayable.rb
@@ -0,0 +1,13 @@
+module Displayable
+  class DisplayText
+    def instructions
+      # TODO
+    end
+  end
+
+  class DisplayBoard
+    def self.display_board_interface
+      # Implementation Here
+    end
+  end
+end
