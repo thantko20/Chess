8231fb16180b008417457750252807be35555cf4
Thant Ko Zaw <thantmkou10@gmail.com>
2021-11-03 20:21:26 +0630

    Add prototype methods

---
+++ lib/board.rb
@@ -0,0 +1,27 @@
+class Board
+  def initialize
+    @position_grid = set_default_postion
+  end
+
+  def set_default_postion
+    # Set all the pieces to starting positions
+    # 8x8 grid
+    default_array = Array.new(8) { Array.new(8) { nil } }
+    
+    # Set default positions for white pieces
+    default_array[0] = nil # RKBQKBKR
+    default_array[1] = nil # PPPPPPPP
+    
+    # Set default positions for black pieces
+    default_array[6] = nil # rkbqkbkr
+    default_array[7] = nil # pppppppp
+
+    default_array
+  end
+
+  def update_board
+    # Update the board
+    # Will get the piece and destroy its current postion,
+    # and update its new position
+  end
+end
