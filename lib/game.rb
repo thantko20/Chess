cf5a611e99aca6e6e06f25efb7623e4372ad1eb7
Thant Ko Zaw <thantmkou10@gmail.com>
2021-11-03 20:10:34 +0630

    Add some prototype methods

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,33 @@
 class Game
   def initialize
-    
+    @player1 = nil
+    @player2 = nil
+    @board = nil
+    @curr_player = nil
+  end
+
+  def play
+    # Intro implementation
+    # Game Loop Implementation
+  end
+
+  def game_loop
+    # Program Logic
+    # Keep track of current player
+    # Select the piece implementation
+    # Move the piece implementation
+    # Check and Checkmate Implementaion
+  end
+
+  def select_piece
+    # Loop program
+    # Based on current player's colour and piece's position,
+    # run the method selecting the piece and
+    # returning the position
+  end
+
+  def move_piece
+    # Ask the player where to move on the board
+    # Allow only legit moves and set the new postion on the board
   end
 end
