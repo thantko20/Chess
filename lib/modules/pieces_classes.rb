038e666b271345079da9c29e38d934cfa2d40de6
Thant Ko Zaw <thantmkou10@gmail.com>
2021-11-03 21:36:52 +0630

    Added Pieces

---
+++ lib/modules/pieces_classes.rb
@@ -0,0 +1,15 @@
+require_relative "../main_piece_class"
+
+module Pieces
+  class King; end
+
+  class Queen; end
+
+  class Rook; end
+
+  class Bishop; end
+
+  class Knight; end
+
+  class Pawn; end
+end
