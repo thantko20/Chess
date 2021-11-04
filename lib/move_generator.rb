1ee9404c1a50ee6693d7b24da7fe4a683012f2e1
Thant Ko Zaw <thantmkou10@gmail.com>
2021-11-04 10:50:12 +0630

    Initial commit

---
+++ lib/move_generator.rb
@@ -0,0 +1,25 @@
+class MoveGenerator
+  def generate_legal_moves(piece, grid)
+    # TODO
+    # Use current piece type and its position
+    # to generate legal moves
+  end
+
+  def generate_king_moves(piece, grid)
+    
+  end
+
+  private
+
+  def opponent_piece?
+    
+  end
+
+  def empty_square?
+    
+  end
+
+  def ally_piece?
+    
+  end
+end
